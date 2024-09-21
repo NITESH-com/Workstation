f5dfa88f0598662e374eaba5edef59ba1578e5f6
NITESH-com <niteshkundarge2004@gmail.com>
2024-09-21 13:49:57 +0530

    1st code
    latest

---
+++ input.py
@@ -0,0 +1,22 @@
+name=input("Enter your name:")
+print("Wellcome",name)
+
+#addiction of two number 
+a=int(input("Enter frist value:"))
+b=int(input("Enter second value:"))
+print("addiction of two number:",a+b)
+
+#Rectangle aera
+a=int(input("Enter lenght value:"))
+b=int(input("Enter width value:"))
+print("Rectangle aera:",0.5*a*b)
+
+# Addictio of three number
+
+
+
+
+
+
+
+
